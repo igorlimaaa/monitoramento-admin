6305e8edc443331109cddc7f3a315ec73277a92f
Igor de Almeida Lima <igor.lima@MacBook-Pro-de-Igor.local>
2021-07-07 15:07:17 -0300

    Implementando segurança

--- src/main/java/br/com/caelum/springbootadmin/SpringBootAdminApplication.java
+++ src/main/java/br/com/caelum/springbootadmin/SpringBootAdminApplication.java
@@ -3,12 +3,14 @@ package br.com.caelum.springbootadmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
+@Import({SecurityConfigurations.class})
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
